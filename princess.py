b34fd2019068bf794092ffd766c079dadf4a4e5e
Cessaya <atamoyanshi@gmail.com>
2024-08-12 17:00:46 +0100

    Adding princess.py

---
+++ princess.py
@@ -0,0 +1,34 @@
+#print('hello world')
+# print(2**2)
+# print(5/2)
+# print(5//2)
+#strings
+# print('hi princessa')
+# variables
+# red_bucket='princess' 
+# print('red_bucket')
+# red_bucket= 10
+# print(red_bucket)
+# print(type(red_bucket))
+# red_bucket= input('what do you want to put in it?') 
+# print('red_bucket')
+
+# CONDITIONAL STATEMENTS
+# print(5==4)
+# ADDING CONDITIONAL STATEMENT AND VARIABLE
+# jason_age= 3
+# age_at_kindergaten= 5
+# print(jason_age == age_at_kindergaten)
+
+# if jason_age < age_at_kindergaten:
+#      print("should jason be in preschool")
+
+# class variables
+class Engineer:
+     name='Mechanical'
+
+Automobile= Engineer()
+print(Automobile.name)
+
+
+
